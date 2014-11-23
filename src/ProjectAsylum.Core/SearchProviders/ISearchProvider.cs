5fa16c9c521cbba299f5e09c3f7ad6b692bb3cad
David Ford <davidford1996@hotmail.co.uk>
2014-11-23 18:59:06 +0000

    init

---
+++ src/ProjectAsylum.Core/SearchProviders/ISearchProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectAsylum.Core.SearchProviders
+{
+    public interface ISearchProvider
+    {
+        bool RssSupport { get; set; } 
+        bool SearchSupport { get; set; }
+        ProviderType ProviderType { get; set; }
+    }
+}
---
+++ src/ProjectAsylum.Core/SearchProviders/ProviderType.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectAsylum.Core.SearchProviders
+{
+    public enum ProviderType
+    {
+        Unknown = 0,
+        Torrent = 1,
+        Usernet = 2
+    }
+}
